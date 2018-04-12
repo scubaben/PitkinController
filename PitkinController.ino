dc0cc7a42b3d96be8cbd99f489e631f3a420dad3
scubaben <scubaben@users.noreply.github.com>
2018-04-11 22:23:33 -0700

    pedantic changes that have no effect on the end result.

--- PitkinController.ino
+++ PitkinController.ino
@@ -1,7 +1,12 @@
 #define switchPin 0
 #define pwmPin 1
-int mode = 0;
-unsigned long timeStamp = 0;
+int powerLevel = 0;
+unsigned long lastTime = 0;
+
+//Output levels for the PWM: 100%, 80%, 60% and 30%
+const int output[] = { 255, 204, 153, 77 };
+
+const int lowestLevel = sizeof(output) / sizeof(int) - 1;
 
 void setup()
 {
@@ -12,38 +17,52 @@ void setup()
 
 void loop()
 {
-	//get currentTime (since on) at the start of each loop
 	unsigned long currentTime = millis();
 
-	//when the switchPin is LOW the switch is in the ON position
-	while (digitalRead(switchPin) == LOW) {
-		setPowerLevel(mode);
+	while (isSwitchOn()) {
+		setPowerLevel(powerLevel);
 
-		//if the switch has been off for more than 10 seconds reset to mode 0;
-		if ((currentTime - timeStamp) > 10000) {
-			mode = 0;
+		unsigned long offTime = currentTime - lastTime;
+
+		if (offTime > 10000) {
+			reset();
+		}
+		else if (offTime > 200) {
+			powerLevel++;
 		}
 
-		//else if the switch has been off for more than 200 milliseconds advance to the next mode
-		else if ((currentTime - timeStamp) > 200) {
-			mode++;
+		if (isLowestLevel(powerLevel)) {
+			powerLevel = lowestLevel;
 		}
 
-		//if already in mode 3 (the lowest setting), stay there
-		if (mode > 3) {
-			mode = 3;
-		}
-
-		//update the timeStamp each time you reach the end of the while loop
-		timeStamp = millis();
+		lastTime = millis();
 	}
 
 	//when the switchPin goes high and the while loop exits, immediately turn the PWM off
-	setPowerLevel(0);
+	turnOff();
+}
+
+bool isSwitchOn() {
+	return digitalRead(switchPin) == LOW;
+}
+
+bool isLowestLevel(int level) {
+	return level >= lowestLevel;
+}
+
+void reset() {
+	powerLevel = 0;
 }
 
 void setPowerLevel(int level) {
-	//Output levels for the PWM: 100%, 80%, 60% and 30%
-	const int output[] = { 255, 204, 153, 77 };
-	analogWrite(pwmPin, output[level]);
+	if (level < 0) {
+		analogWrite(pwmPin, 0);
+	}
+	else {
+		analogWrite(pwmPin, output[level]);
+	}
+}
+
+void turnOff() {
+	setPowerLevel(-1);
 }
